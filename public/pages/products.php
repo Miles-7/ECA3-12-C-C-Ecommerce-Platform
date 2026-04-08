3e800b03aa689d68e7bd47f2437feb504788bb63
Miles-7 <janniemiles9999@gmail.com>
2026-04-08 11:20:17 +0200

    refined pages

--- public/pages/products.php
+++ public/pages/products.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Page Title Here</title>
     <link rel="stylesheet" href="styling/main.css">
 </head>
+
 <body>
 
-<?php
-// Include header with navigation
-require __DIR__ . '/../../src/components/header.php';
-?>
+    <?php
+    // Include header with navigation
+    require __DIR__ . '/../../src/components/header.php';
+    ?>
 
-<main>
-    <!-- Your page-specific content here -->
-    <h2>Products</h2>
-    
-</main>
+    <main>
+        <!-- Your page-specific content here -->
+        <h2>Products</h2>
+
+    </main>
 
 
 
 </body>
+
 </html>
