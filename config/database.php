dfc448f84b715f3a39c474ce989afb157a6dad6f
Miles-7 <janniemiles9999@gmail.com>
2026-06-03 22:11:56 +0200

    updated db connection credentials

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$host   = "localhost";
-$dbname = "vuka_db";    // my database's name in phpMyAdmin
-$user   = "root";
-$pass   = "";
+$host   = "sql109.infinityfree.com";
+$dbname = "if0_42090951_vuka_db";    // my database's name in phpMyAdmin
+$user   = "if0_42090951";
+$pass   = "HondaXr1622";
 
 $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
